49c4b26f04594128b0ed21da7579c07f0119f658
arsen <asmandiyarov_ars@mail.ru>
2021-02-12 14:08:48 +0500

    HW3

--- client.py
+++ client.py
@@ -1,20 +1,23 @@
-from socket import socket, AF_INET, SOCK_STREAM
-import sys
+from utils import get_ip_and_port
 
-if '-a' in sys.argv:
-    SERV_IP = sys.argv[sys.argv.index('-a') + 1]
-else:
-    SERV_IP = 'localhost'
 
-if '-p' in sys.argv:
-    PORT = int(sys.argv[sys.argv.index('-p') + 1])
-else:
-    PORT = 7777
+def main():
+    try:
+        port, serv_ip, s = get_ip_and_port()
+        s.connect((serv_ip, port))
+        msg = 'Привет, сервер!'
+        s.send(msg.encode('utf-8'))
+        data = s.recv(4096)
+        s.close()
+        try:
+            print(data.decode('utf-8'))
+        except UnicodeDecodeError:
+            print('Сообщение от сервера принято в другой кодировке, расшифровать не удалось')
+    except ConnectionRefusedError:
+        print("Возможно вы ошиблись адресом подключения, проверьте передаваемые параметры!")
+    except TypeError:
+        print("Проверьте правильность введенных данных!")
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((SERV_IP, PORT))
-msg = 'Привет, сервер!'
-s.send(msg.encode('utf-8'))
-data = s.recv(4096)
-s.close()
-print(data.decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,27 +1,24 @@
-import sys
-from socket import socket, AF_INET, SOCK_STREAM
 import time
+from utils import get_ip_and_port
 
 
-if '-a' in sys.argv:
-    SERV_IP = sys.argv[sys.argv.index('-a') + 1]
-else:
-    SERV_IP = ''
+PORT, SERV_IP, s = get_ip_and_port(server=True)
 
-if '-p' in sys.argv:
-    PORT = int(sys.argv[sys.argv.index('-p') + 1])
-else:
-    PORT = 7777
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind((SERV_IP, PORT))
-s.listen(1)
+def main():
+    while True:
+        client, addr = s.accept()
+        print(f'Получен запрос на соединение от {addr}')
+        msg_from_client = client.recv(4096)
+        try:
+            print(msg_from_client.decode('utf-8'))
+        except UnicodeDecodeError:
+            print('Сообщение не удалось расшифровать')
+        timestr = time.ctime(time.time()) + '\n' + "привет клиент!"
+        client.send(timestr.encode('utf-8'))
+        client.close()
 
-while True:
-    client, addr = s.accept()
-    print(f'Получен запрос на соединение от {addr}')
-    msg_from_client = client.recv(4096)
-    print(msg_from_client.decode('utf-8'))
-    timestr = time.ctime(time.time()) + '\n' + "привет клиент!"
-    client.send(timestr.encode('utf-8'))
-    client.close()
+
+if __name__ == "__main__":
+    main()
+
