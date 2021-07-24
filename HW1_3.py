d3c80a9ca6c3b4d862651cc6d7286d9c251f2df6
arsen191 <62110458+arsen191@users.noreply.github.com>
2021-07-24 19:06:16 +0500

    Delete HW1_3.py

--- HW1_3.py
+++
@@ -1,7 +0,0 @@
-"""Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе."""
-w1 = b'attribute'
-#w2 = b'класс'   # SyntaxError: bytes can only contain ASCII literal characters.
-#w3 = b'функция' # SyntaxError: bytes can only contain ASCII literal characters.
-w4 = b'type'
-
-print(w1, w4)
