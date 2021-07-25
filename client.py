788196a19c35d6013d08b688946d8df46b559695
arsen <asmandiyarov_ars@mail.ru>
2021-07-25 16:52:52 +0500

    переписал под ооп, добавил метакласс для сервера/клиента и дескриптор для валидации порта сервера

--- client.py
+++ client.py
@@ -3,78 +3,81 @@ import logging
 import sys
 import threading
 import time
-from log_decorator import Log
-from utils import get_ip_and_port
+from utils import get_ip_and_port, Verifier
 
 logger = logging.getLogger('client_log')
 
 
-@Log()
-def main():
-    port, serv_ip, sock = get_ip_and_port()
-    sock.connect((serv_ip, port))
-    person = input('Введите свое имя: ')
-    create_presence_message(person, sock)
-    if json.loads(sock.recv(4096).decode('utf-8')).get('response') == 200:
-        print('Подключение к серверу установлено!')
+class Client(metaclass=Verifier):
+    """
+    Клиентская часть
+    """
 
-    try:
-        sender = threading.Thread(target=write_msg, args=(sock, person, ), daemon=True)
-        receiver = threading.Thread(target=read_and_parse_msg, args=(sock, ), daemon=True)
+    def __init__(self, port, serv_ip, sock):
+        self.port = port
+        self.serv_ip = serv_ip
+        self.sock = sock
+        self.person = input('Введите свое имя: ')
+
+    def main(self):
+        self.sock.connect((self.serv_ip, self.port))
+        # self.create_presence_message()
+        self.read_and_parse_msg()
+
+        sender = threading.Thread(target=self.write_msg, args=(), daemon=True)
+        receiver = threading.Thread(target=self.read_and_parse_msg, args=(), daemon=True)
         sender.start()
         receiver.start()
-    except KeyboardInterrupt:
-        sock.close()
-    else:
+
         while True:
             if sender.is_alive() and receiver.is_alive():
                 continue
+            self.sock.close()
             break
 
+    def write_msg(self):
+        while True:
+            msg = input('')
+            self.sock.send(self.handle_response(msg).encode('utf-8'))
 
-def write_msg(sock, frm):
-    while True:
-        msg = input('')
-        sock.send(handle_response(msg, frm).encode('utf-8'))
+    def read_and_parse_msg(self):
+        while True:
+            try:
+                data = json.loads(self.sock.recv(4096).decode('utf-8'))
+                if data.get('action') == 'probe':
+                    self.create_presence_message()
+                elif data.get('response') == 200:
+                    print('Подключение к серверу установлено!')
+                    break
+                elif data.get('action') == 'msg':
+                    print(f"\rResponse: {data.get('message')}")
+            except KeyboardInterrupt:
+                print('Клиентское приложение закрыто')
+                sys.exit(1)
 
-
-def read_and_parse_msg(sock, presence=None):
-    while True:
-        try:
-            data = json.loads(sock.recv(4096).decode('utf-8'))
-            if data.get('action') == 'probe':
-                sock.send(presence)
-            elif data.get('response') == 200:
-                print('Подключение к серверу установлено!')
-            elif data.get('action') == 'msg':
-                print(f"\rResponse: {data.get('message')}")
-        except KeyboardInterrupt:
-            print('Клиентское приложение закрыто')
-            sys.exit(1)
-
-
-def create_presence_message(user, sock):
-    message = {
-        "action": "presence",
-        "time": time.time(),
-        "type": "status",
-        "user": {
-            "account_name": user,
-            "status": "I am here!"
-        }
-    }
-    sock.send(json.dumps(message).encode('utf-8'))
-
-
-def handle_response(message, frm):
-    return json.dumps({
-            "action": "msg",
+    def create_presence_message(self):
+        message = {
+            "action": "presence",
             "time": time.time(),
-            "to": "client",
-            "from": frm,
-            "message": message
-        })
+            "type": "status",
+            "user": {
+                "account_name": self.person,
+                "status": "I am here!"
+            }
+        }
+        self.sock.send(json.dumps(message).encode('utf-8'))
+
+    def handle_response(self, message):
+        return json.dumps({
+                "action": "msg",
+                "time": time.time(),
+                "to": "client",
+                "from": self.person,
+                "message": message
+            })
 
 
 if __name__ == '__main__':
-    main()
+    p, s_ip, s = get_ip_and_port()
+    client = Client(p, s_ip, s)
+    client.main()
--- server.py
+++ server.py
@@ -2,71 +2,103 @@ import json
 import select
 import time
 import logging
-from log_decorator import Log
-from utils import get_ip_and_port
+from log.log_decorator import Log
+from utils import get_ip_and_port, Verifier, CONFIGS
 
 logger = logging.getLogger('server_log')
 
 
-def read_requests(r_list, clients):
-    responses = {}
-    for sock in r_list:
-        try:
-            data = sock.recv(4096).decode('utf-8')
-            responses[sock] = handle_message(data)
-            print(responses)
-        except:
-            clients.remove(sock)
-    return responses
+class PortValidator:
+
+    def __init__(self, default=CONFIGS['PORT']):
+        self.default = default
+        self._value = None
+
+    def __get__(self, instance, owner):
+        return self._value or self.default
+
+    def __set__(self, instance, value):
+        if isinstance(value, int) and 1024 <= value <= 65535:
+            self._value = value
+            return
+        raise ValueError('Значение порта должно быть целым числом, больше 1024 и меньше 65535')
 
 
-def write_responses(requests, w_list, clients):
-    for sock in w_list:
-        for _, req in requests.items():
+class Server(metaclass=Verifier):
+    """
+    Серверная часть
+    """
+    port = PortValidator()
+
+    def __init__(self, sock):
+        self.sock = sock
+        self.clients = []
+
+    @Log()
+    def main(self):
+        print("server started!")
+        while True:
             try:
-                resp = req.encode('utf-8')
-                sock.send(resp)
-            except:
-                sock.close()
-                clients.remove(sock)
-
-
-@Log()
-def main():
-    clients = []
-    sock = get_ip_and_port(server=True)
-    while True:
-        try:
-            client, addr = sock.accept()
-            print(f'Получен запрос на соединение от {addr}')
-            client.send(json.dumps({"action": "probe", "time": time.time()}).encode('utf-8'))
-            clients.append(client)
-        except OSError:
-            pass
-        finally:
-            r_list, w_list = [], []
-            try:
-                r_list, w_list, e_list = select.select(clients, clients, [], 1)
-            except:
+                client, addr = self.sock.accept()
+                print(f'Получен запрос на соединение от {addr}')
+                client.send(json.dumps({"action": "probe", "time": time.time()}).encode('utf-8'))
+                self.clients.append(client)
+            except OSError:
                 pass
+            finally:
+                r_list, w_list = [], []
+                try:
+                    r_list, w_list, e_list = select.select(self.clients, self.clients, [], 0)
+                except:
+                    pass
 
-            requests = read_requests(r_list, clients)
-            if requests:
-                write_responses(requests, w_list, clients)
+                requests = self.read_requests(r_list, self.clients)
+                if requests:
+                    self.write_responses(requests, w_list)
 
+    def read_requests(self, r_list, clients):
+        responses = {}
+        for sock in r_list:
+            try:
+                data = sock.recv(4096).decode('utf-8')
+                responses[sock] = self.handle_message(data)
+                print(responses)
+            except:
+                clients.remove(sock)
+        return responses
 
-def handle_message(msg):
-    message = json.loads(msg)
-    if message.get('action') == 'presence' and message.get('time') and message.get('type') and message.get('user'):
-        return json.dumps({'response': 200, 'time': time.time(), 'alert': 'OK'})
-    elif message.get('action') == 'msg' and message.get('time') and message.get('to') and message.get('from') \
-            and message.get('message'):
-        return json.dumps(
-            {'action': 'msg', 'time': time.time(), 'to': message.get('to'), 'from': message.get('from'), "message": message.get('message')})
-    else:
-        return json.dumps({'response': 400, 'time': time.time(), 'error': 'неправильный запрос/JSON-объект'})
+    def write_responses(self, requests, w_list):
+        for sock in w_list:
+            for _, req in requests.items():
+                try:
+                    if _ is sock and 'response' in req:
+                        resp = req.encode('utf-8')
+                        sock.send(resp)
+                    elif 'response' in req:
+                        continue
+                    else:
+                        resp = req.encode('utf-8')
+                        sock.send(resp)
+                except:
+                    sock.close()
+                    self.clients.remove(sock)
+
+    def handle_message(self, msg):
+        message = json.loads(msg)
+        if message.get('action') == 'presence' and message.get('time') and message.get('type') and message.get('user'):
+            return json.dumps({'response': 200, 'time': time.time(), 'alert': 'OK'})
+        elif message.get('action') == 'msg' and message.get('time') and message.get('to') and message.get('from') \
+                and message.get('message'):
+            return json.dumps(
+                {'action': 'msg', 'time': time.time(), 'to': message.get('to'), 'from': message.get('from'),
+                 "message": message.get('message')})
+        else:
+            return json.dumps({'response': 400, 'time': time.time(), 'error': 'неправильный запрос/JSON-объект'})
 
 
 if __name__ == "__main__":
-    print("server started!")
-    main()
+    p, s = get_ip_and_port(server=True)
+    server = Server(s)
+    server.port = p
+    server.main()
+
